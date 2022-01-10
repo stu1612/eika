06d818fc670d27a03dc2d7db7d299740b5a3a5d0
Stu Bolderson <stu.bolderson@aol.com>
2022-01-10 17:59:42 +0100

    jsx and styling added to home.js basic elements added - not mobile first and css classes need to be refactored to be re-used for task jsx elements section grid layout is good which can be used for all pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
 import { Modal } from "../components/Modal";
+import img from "../assets/images/header-img.png";
 
 export const Home = () => {
   const [openModal, setOpenModal] = useState(false);
   return (
-    <div className="home">
-      <h2>Home</h2>
-      <button onClick={() => setOpenModal(true)}>Add Task</button>
+    <section className="grid-container" id="home">
+      <div className="home home-image__container">
+        <img src={img} alt="online task list" className="home-image" />
+      </div>
+      <div className="home-content__title">
+        <h2>Shopping List</h2>
+      </div>
+      <div className="home-content__description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+          corrupti quibusdam aliquam possimus error molestiae sunt nisi
+          accusantium dolore vero.
+        </p>
+      </div>
+      <div className="btn-container">
+        <button onClick={() => setOpenModal(true)}>Add Task</button>
+      </div>
       {openModal ? <Modal /> : null}
-    </div>
+    </section>
   );
 };
